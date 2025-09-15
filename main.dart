94015b6ae128983a497ef05a91b8f7ccdea9c8ea
Joms <168385453+gliocamjomar@users.noreply.github.com>
2025-09-15 01:14:45 +0000

    Building your first flutter profile page

---
+++ main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context){
+
+    return MaterialApp(
+      title: 'My Profile App',
+      theme: ThemeData(primarySwatch: Colors.blue,
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('Profile'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //profile picture
+            Container(
+            width: 160,
+            height: 160,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 4,
+
+                ),
+                  boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: ClipOval(
+                child: Image.network(
+                'https://static.vecteezy.com/system/resources/previews/011/311/302/large_2x/jg-logo-jg-design-blue-and-red-jg-letter-jg-letter-logo-design-initial-letter-jg-linked-circle-uppercase-monogram-logo-vector.jpg',
+                width: 160,
+                height: 160,
+                fit: BoxFit.cover,
+                )
+              ),
+            ),
+            //name
+            SizedBox(height: 20),
+            Text(
+              'Global Reciprocal Colleges',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ) ,
+            ),
+            //job title / subtitle
+            SizedBox(height: 8),
+            Text(
+                'Colleges of Computer Studies',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.blue,
+              fontWeight: FontWeight.w500,
+            ),
+            ),
+
+            //about me section
+
+            SizedBox(height: 30),
+            Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 8,
+                    offset: Offset(0, 3),
+                   ),
+                 ],
+               ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                        'Program Description',
+                    style: TextStyle(
+                    fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                        'I am a passionate programmer with a strong foundation in coding and problem-solving. i enjoy building application, learning new technologies and creating efficient solution.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[700],
+                      height:  1.5,
+                    ),
+                     textAlign: TextAlign.left,
+                    ),
+                  ],
+                ),
+             ),
+           ],
+        ),
+      ),
+    );
+  }
